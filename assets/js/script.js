28d2e30ecf3ae84ed1c2a3fcd01bf283c108422b
Lucas <lucass20586@gmail.com>
2021-12-12 11:06:02 -0300

    edição do tema

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,22 @@
+var img = document.getElementById("userPhoto");
+
+
 function mostrar(e) {
     if (e.classList.contains("fa-moon-o")) { //se tem lua
         e.classList.remove("fa-moon-o"); //remove classe icone lua
         e.classList.add("fa-sun-o"); //coloca classedo sol
         e.style.color = "rgb(225, 225, 0)";
-        document.body.style.background = 'rgb(10, 10, 10)';
+        document.body.style.background = 'rgb(24, 28, 75)';
         document.querySelector('#userName').style.color = '#fff';
-
+        
+        document.getElementById("userPhoto").src = "/logo-2.jpeg"
+        
         let links = document.querySelectorAll('.link');
         // Pega apenas o primeiro do array
-        links[0].style.filter = 'grayscale(100%)';
+        links[0].style.background = 'white';
+        links[0].style.color = 'black';
         // pega todos
-        for (let i = 0; i < links.length; i++) {
+        for (let i = 1; i < links.length; i++) {
             links[i].style.filter = 'grayscale(100%)';
         }
 
@@ -26,10 +32,14 @@ function mostrar(e) {
         e.classList.remove("fa-sun-o"); //remove classe icone lua
         e.classList.add("fa-moon-o"); //coloca classedo sol
         e.style.color = "#585858";
-        document.body.style.background = 'rgb(243, 242, 242)';
-        document.querySelector('#userName').style.color = 'rgb(99, 99, 99)';
+        document.body.style.background = 'rgb(254, 254, 254)';
+        document.querySelector('#userName').style.color = '';
+
+        document.getElementById("userPhoto").src = "/logo.jpeg"
 
         let links = document.querySelectorAll('.link');
+        links[0].style.background = 'rgb(24, 28, 75)';
+        links[0].style.color = 'white';
         // Pega apenas o primeiro do array
         links[0].style.filter = 'grayscale(0%)';
         // pega todos
